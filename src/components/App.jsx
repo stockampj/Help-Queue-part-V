88552599431480ab23e613c2b005b05ea8346aa4
Student <example@example.com>
2019-11-21 13:55:00 -0800

    fix errors in handleAddingNewTicketToList in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import Moment from 'moment';
 import Admin from './Admin';
+import { v4 } from 'uuid';
 
 class App extends React.Component {
 
@@ -24,11 +25,11 @@ class App extends React.Component {
   }
 
   handleAddingNewTicketToList(newTicket) {
-
+    var newTicketId = v4();
     var newMasterTicketList = Object.assign({}, this.state.masterTicketList, {
-      [newTicket.id]: newTicket
+      [newTicketId]: newTicket
     });
-    newMasterTicketList[newTicket.id].formattedWaitTime = newMasterTicketList[newTicket.id].timeOpen.fromNow(true);
+    newMasterTicketList[newTicketId].formattedWaitTime = newMasterTicketList[newTicketId].timeOpen.fromNow(true);
     this.setState({masterTicketList: newMasterTicketList});
   }
 
@@ -53,6 +54,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.state.masterTicketList);
     return (
       <div>
         <Header />
